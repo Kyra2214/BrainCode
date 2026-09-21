19418d9ec80415935e517430b3325f94d7004e62
Alexandre <xandelsw@gmail.com>
2026-09-21 10:32:07 -0300

    test: corrigir contrato do ValidationResult no Roadmap

--- brain/src/test/kotlin/com/brain/core/RoadmapValidationCoordinatorTest.kt
+++ brain/src/test/kotlin/com/brain/core/RoadmapValidationCoordinatorTest.kt
@@ -12,17 +12,17 @@ class RoadmapValidationCoordinatorTest {
     )
     @Test
     fun pass_aprova_tarefa_responsavel() {
-        val result = ValidationResult(ValidationStatus.PASS, "c", "cap", taskId = "t1")
+        val result = ValidationResult(ValidationStatus.PASS, "c", taskId = "t1", capability = "cap")
         assertEquals(TarefaStatus.APROVADA, RoadmapValidationCoordinator.apply(roadmap(), result).fases[0].modulos[0].submodulos[0].tarefas[0].statusAtual)
     }
     @Test
     fun fail_reprova_tarefa_responsavel() {
-        val result = ValidationResult(ValidationStatus.FAIL, "c", "cap", taskId = "t1")
+        val result = ValidationResult(ValidationStatus.FAIL, "c", taskId = "t1", capability = "cap")
         assertEquals(TarefaStatus.REPROVADA, RoadmapValidationCoordinator.apply(roadmap(), result).fases[0].modulos[0].submodulos[0].tarefas[0].statusAtual)
     }
     @Test
     fun needs_input_nao_avanca_tarefa() {
-        val result = ValidationResult(ValidationStatus.NEEDS_INPUT, "c", "cap", taskId = "t1")
+        val result = ValidationResult(ValidationStatus.NEEDS_INPUT, "c", taskId = "t1", capability = "cap")
         assertEquals(TarefaStatus.PENDENTE, RoadmapValidationCoordinator.apply(roadmap(), result).fases[0].modulos[0].submodulos[0].tarefas[0].statusAtual)
     }
 }
